71f1df3d649e07a89da8f7db9edc4c8711d49fbd
Lidya Ghilay <lidyahenok92gmail.com>
2024-04-15 15:59:31 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let messages = [];
+    for (let name of names) {
+      messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
+  
